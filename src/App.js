fb2b400b77b51995dd527b6f248aefc282b56751
Joshua Sands <josh.sands@mail.com>
2022-12-16 14:08:20 -0600

    started button functions

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
-import React, {useState} from 'react';
-import './App.css';
-import ExpenseList from './components/ExpenseList';
-import ExpenseForm from './components/ExpenseForm';
-import Alert from './components/Alert';
-import { v4 as uuid } from 'uuid';
+import React, { useState } from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import Alert from "./components/Alert";
+import { v4 as uuid } from "uuid";
 
 const initialExpenses = [
-  {id:uuid(), charge:"rent", amount:1600},
-  {id:uuid(), charge:"car payment", amount:400},
-  {id:uuid(), charge:"credit", amount:1400}
+  { id: uuid(), charge: "rent", amount: 1600 },
+  { id: uuid(), charge: "car payment", amount: 400 },
+  { id: uuid(), charge: "credit", amount: 1400 },
 ];
 
-
 function App() {
-const [expenses, setExpenses] = useState(initialExpenses);
+  // *** STATE VALUES ***
+  // ALL EXPENSES, ADD EXPENSE
+  const [expenses, setExpenses] = useState(initialExpenses);
+  // single expense
+  const [charge, setCharge] = useState("");
+  // single amount
+  const [amount, setAmount] = useState("");
+  // *** FUNCTIONALITY ***
 
-
-  return <>
-  <Alert />
-    <h1>budget calculator</h1>
-    <main className='App'>
-  <ExpenseForm />
-  <ExpenseList expenses={expenses}/>
-  </main>
-  <h1>
-    total spending : <span className='total'>
-      $
-      {expenses.reduce((acc,curr)=>{
-        return (acc + curr.amount)
-      },0)}
-    </span>
-  </h1>
-  </>
+  return (
+    <>
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending :{" "}
+        <span className="total">
+          $
+          {expenses.reduce((acc, curr) => {
+            return acc + curr.amount;
+          }, 0)}
+        </span>
+      </h1>
+    </>
+  );
 }
 
 export default App;
