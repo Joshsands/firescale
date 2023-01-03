44d4a8ec33a818cb4c9026d0e91f64b6ddecee35
Joshua Sands <josh.sands@mail.com>
2023-01-03 14:44:56 -0600

    added about me and contact section:

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
+import Pricing from './pages/Pricing';
+import Service from './pages/Service';
+import Contact from './pages/Contact';
 import Navigation from './components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,6 +18,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="/shop" element={<Shop/>} />
+            <Route path="/pricing" element={<Pricing/>} />
+            <Route path="/service" element={<Service/>} />
+            <Route path="/contact" element={<Contact/>} />
+
           </Routes>
         </div>
     </Router>
---
+++ src/pages/Pricing.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Pricing = () => {
+  return (
+    <div>
+      THIS IS THE PRICING PAGE
+    </div>
+  );
+};
+
+export default Pricing;
