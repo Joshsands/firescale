5f6d67c558b211d9013e316a242228e3073258bb
Joshua Sands <83298011+Joshsands@users.noreply.github.com>
2023-01-08 18:24:34 -0600

    added local storage memory

--- src/components/Estimatortool/index.js
+++ src/components/Estimatortool/index.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Label, Input, Select, Textarea, Radio, Checkbox } from "@rebass/forms";
 import { Box, Flex } from "rebass";
 import { Switch, Button } from 'evergreen-ui'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas, faSave, faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const blank = 0;
 const FACP = 1500;
@@ -10,8 +13,8 @@ const docBox = 300;
 const DACT = 200;
 
 const Estimatortool = () => {
-  
-  {/* ROW 1 */}
+
+  {/* ROW 1 */ }
   const [price1, setPrice1] = useState(0);
   const [count1, setCount1] = useState(0);
   const [subtotal1, setSubtotal1] = useState(0);
@@ -20,21 +23,23 @@ const Estimatortool = () => {
   const [selectedOption1, setSelectedOption1] = useState(0);
   const [markupPercent, setMarkupPercent] = useState(0.3);
 
+
+
   function handleOptionChange1(event) {
     let number = parseFloat(event.target.value)
     setSelectedOption1(number)
     setPrice1(number)
   }
-  
+
   function handleClick1() {
     if (price1 === blank) {
       return
     }
     else {
-    setCount1(count1 + 1);
-    setSubtotal1((count1 + 1) * price1);
-    setMarkup1((subtotal1 + price1) * markupPercent);
-    setTotal1((subtotal1 + price1) + (markup1 + (price1 * markupPercent)));
+      setCount1(count1 + 1);
+      setSubtotal1((count1 + 1) * price1);
+      setMarkup1((subtotal1 + price1) * markupPercent);
+      setTotal1((subtotal1 + price1) + (markup1 + (price1 * markupPercent)));
     }
   }
 
@@ -43,20 +48,20 @@ const Estimatortool = () => {
       return
     }
     else {
-    setCount1(count1 - 1);
-    setSubtotal1((count1 - 1) * price1);
-    setMarkup1((subtotal1 - price1) * markupPercent);
-    setTotal1((subtotal1 - price1) - (markup1 - (price1 * markupPercent)))
+      setCount1(count1 - 1);
+      setSubtotal1((count1 - 1) * price1);
+      setMarkup1((subtotal1 - price1) * markupPercent);
+      setTotal1((subtotal1 - price1) - (markup1 - (price1 * markupPercent)))
     }
   }
 
   const formattedSubtotal = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(subtotal1);
   const formattedMarkup = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(markup1);
-  const formattedTotal= new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(total1);
-  const formattedPrice= new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price1);
+  const formattedTotal = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(total1);
+  const formattedPrice = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price1);
 
 
-  {/* ROW 2 */}
+  {/* ROW 2 */ }
   const [price2, setPrice2] = useState(0);
   const [count2, setCount2] = useState(0);
   const [subtotal2, setSubtotal2] = useState(0);
@@ -75,10 +80,10 @@ const Estimatortool = () => {
       return
     }
     else {
-    setCount2(count2 + 1);
-    setSubtotal2((count2 + 1) * price2);
-    setMarkup2((subtotal2 + price2) * markupPercent);
-    setTotal2((subtotal2 + price2) + (markup2 + (price2 * markupPercent)));
+      setCount2(count2 + 1);
+      setSubtotal2((count2 + 1) * price2);
+      setMarkup2((subtotal2 + price2) * markupPercent);
+      setTotal2((subtotal2 + price2) + (markup2 + (price2 * markupPercent)));
     }
   }
 
@@ -88,27 +93,72 @@ const Estimatortool = () => {
       return
     }
     else {
-    setCount2(count2 - 1);
-    setSubtotal2((count2 - 1) * price2);
-    setMarkup2((subtotal2 - price2) * markupPercent);
-    setTotal2((subtotal2 - price2) - (markup2 - (price2 * markupPercent)))
+      setCount2(count2 - 1);
+      setSubtotal2((count2 - 1) * price2);
+      setMarkup2((subtotal2 - price2) * markupPercent);
+      setTotal2((subtotal2 - price2) - (markup2 - (price2 * markupPercent)))
+    }
   }
-}
 
 
   const formattedSubtotal2 = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(subtotal2);
   const formattedMarkup2 = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(markup2);
-  const formattedTotal2= new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(total2);
-  const formattedPrice2= new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price2);
+  const formattedTotal2 = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(total2);
+  const formattedPrice2 = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(price2);
 
 
-  {/* TOTAL MATERIAL */}
-  const materialTotal = (total1 + total2)
+
+  {/* TOTAL MATERIAL */ }
+  const [materialTotal, setMaterialTotal] = useState([]);
+
+  function storeMaterial() {
+    setMaterialTotal(total1 + total2);
+    localStorage.setItem('materialTotal', JSON.stringify(materialTotal));
+
+    }
+
+  // useEffect(() => {
+  //   localStorage.setItem('materialTotal', JSON.stringify(materialTotal));
+  // }, []);
+
+  useEffect(() => {
+    const storedMaterial = JSON.parse(localStorage.getItem('materialTotal'));
+  if (storedMaterial) {
+    setMaterialTotal(storedMaterial)
+  }
+  }, []);
+
+console.log(materialTotal)
+
+
+
+
   const formattedMaterialTotal= new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(materialTotal);
-
+ 
   return (
     <>
-      <h1>MATERIAL: {formattedMaterialTotal}</h1> 
+
+<div className="d-flex flex-row">
+{/* <div className="p-0 align-self-center me-1" >
+
+<Button margin={0} appearance="primary" intent="success" onClick={recall}>
+  <FontAwesomeIcon icon={ faPlus } />
+</Button>
+</div> */}
+
+<div className="p-0 align-self-center me-1" >
+
+<Button margin={0} appearance="primary" intent="success" onClick={storeMaterial}>
+<FontAwesomeIcon icon={ faSave } />
+</Button>
+  </div>
+<div className="p-0 align-self-center me-1" >
+
+        <h1>MATERIAL: {formattedMaterialTotal}</h1>
+        </div>
+
+      </div>
+
       <Box as="form" onSubmit={(e) => e.preventDefault()} py={3}>
 
         {/*row 1 */}
@@ -116,12 +166,12 @@ const Estimatortool = () => {
           <Box width={1 / 4} px={1}>
             <Label htmlFor="device">Device</Label>
             <Select id="device" device="device" defaultValue='0' value={selectedOption1} onChange={handleOptionChange1}>
-        <option value={blank}> </option>
-        <option value={FACP}>FACP</option>
-        <option value={NAC}>NAC BOOSTER</option>
-        <option value={docBox}>DOC BOX</option>
-        <option value={DACT}>DACT</option>
-        </Select>
+              <option value={blank}> </option>
+              <option value={FACP}>FACP</option>
+              <option value={NAC}>NAC BOOSTER</option>
+              <option value={docBox}>DOC BOX</option>
+              <option value={DACT}>DACT</option>
+            </Select>
           </Box>
 
           <Box width={1 / 4} px={1}>
@@ -129,21 +179,21 @@ const Estimatortool = () => {
 
             <div className="d-flex flex-row">
 
-            <div className="p-0 align-self-center me-1" >
-            <Button margin={0} appearance="primary" intent="success" onClick={handleClick1}>
-              +</Button>
+              <div className="p-0 align-self-center me-1" >
+                <Button margin={0} appearance="primary" intent="success" onClick={handleClick1}>
+                  +</Button>
               </div>
 
               <div className="p-0 align-self-center me-1">
-              <Button margin={0} appearance="primary" intent="danger" onClick={minuesClick1}>
-              -</Button>
+                <Button margin={0} appearance="primary" intent="danger" onClick={minuesClick1}>
+                  -</Button>
               </div>
 
               <div className="align-self-center">
-              <Input id="qty" qty="qty" value={count1} />
+                <Input id="qty" qty="qty" value={count1} />
               </div>
 
-              </div>
+            </div>
           </Box>
 
           <Box width={1 / 4} px={1}>
@@ -170,29 +220,29 @@ const Estimatortool = () => {
         {/*row 2 */}
         <Flex mx={10} mb={3}>
           <Box width={1 / 4} px={1}>
-          <Select id="device" device="device" value={selectedOption2} onChange={handleOptionChange2}>
-        <option value={blank}> </option>
-        <option value={FACP}>FACP</option>
-        <option value={NAC}>NAC BOOSTER</option>
-        <option value={docBox}>DOC BOX</option>
-        <option value={DACT}>DACT</option>
-        </Select>
+            <Select id="device" device="device" value={selectedOption2} onChange={handleOptionChange2}>
+              <option value={blank}> </option>
+              <option value={FACP}>FACP</option>
+              <option value={NAC}>NAC BOOSTER</option>
+              <option value={docBox}>DOC BOX</option>
+              <option value={DACT}>DACT</option>
+            </Select>
           </Box>
 
           <Box width={1 / 4} px={1}>
-          <div className="d-flex flex-row">
-          <div className="p-0 align-self-center me-1" >
-            <Button margin={0} appearance="primary" intent="success" onClick={handleClick2}>
-              +</Button>
+            <div className="d-flex flex-row">
+              <div className="p-0 align-self-center me-1" >
+                <Button margin={0} appearance="primary" intent="success" onClick={handleClick2}>
+                  +</Button>
               </div>
               <div className="p-0 align-self-center me-1" >
-              <Button margin={0} appearance="primary" intent="danger" onClick={minuesClick2}>
-              -</Button>
+                <Button margin={0} appearance="primary" intent="danger" onClick={minuesClick2}>
+                  -</Button>
               </div>
               <div className="p-0">
 
-            <Input id="qty" qty="qty" value={count2} />
-            </div>
+                <Input id="qty" qty="qty" value={count2} />
+              </div>
             </div>
           </Box>
 
